fddd8293a7a78601818bdea1b4032106b316cdaa
rudraabhilash <rudraabhilash@gmail.com>
2024-11-29 11:28:39 +0530

    write operation

--- hospital_project/payments/urls.py
+++ hospital_project/payments/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('payment', views.payment, name = 'payment'),
-               path('paymentsdata', views.paymentsdata, name = 'paymentsdata'),]
+               path('paymentsdata', views.paymentsdata, name = 'paymentsdata'),
+               path('paymentsdatawrite', views.paymentsdatawrite, name='paymentsdatawrite'),
+               path('paymentsdata', views.paymentsdata, name='paymentsdata'),]
 
--- hospital_project/payments/views.py
+++ hospital_project/payments/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import redirect, render
 from django.template import loader
 from django.http import HttpResponse
 from .models import payments
+from django.views.decorators.csrf import csrf_exempt
 
 def payment(request):
     #return HttpResponse("welcome!")
@@ -17,3 +19,30 @@ def paymentsdata(request):
         'pdata': pdata,
     }
     return HttpResponse(template.render(context, request))
+
+#write page
+def paymentsdata(request):   
+    template = loader.get_template('paymentsdatawrite.html')
+    return HttpResponse(template.render())
+
+#Write operation
+@csrf_exempt
+def paymentsdatawrite(request):
+    if request.method == 'POST':
+        Name = request.POST.get('Name')
+        Remarks = request.POST.get('Remarks')
+        contact = request.POST.get('contact')
+        emailID = request.POST.get('emailID')
+        consulatationFee = request.POST.get('consulatationFee')
+
+        temp = payments(
+            Name=Name,
+            Remarks=Remarks,
+            contact=contact,
+            emailID=emailID,
+            consulatationFee=consulatationFee,
+        )
+        temp.save() #this line is important for saving form data in the database
+        return redirect("/")  # this page after saving data in database
+    else:
+        return HttpResponse("Invalid request method..")
