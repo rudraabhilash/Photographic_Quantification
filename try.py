44881402cdf6a553c9369d3fcf132edb6d1692df
Abhilash Rudra <rudraabhilash@users.noreply.github.com>
2026-01-20 07:37:10 +0000

    try

--- try.py
+++ try.py
@@ -8,41 +8,20 @@
 
 # def update(name, size, price)
 # def getavgprice(name): -> ave price of that name
-class Stockstat:
-
-    def __init__(self):
-        self.name = ''
-        self.size = 0
-        self.price = 0
-        self.stockdict = {}
-        self.sizelot = {}
-    def update(self, name, size,price):
-        self.name = name
-        self.size = size
-        self.price = price
-        cum = 0
-        if self.name in self.stockdict:
-            cum += self.stockdict[self.name] + size*price
-            self.sizelot[self.name] = self.sizelot[self.name]+self.size 
-        else:
-            self.stockdict[self.name] = size*price 
-            self.sizelot[self.name] = size 
+class vwap:
+	def __init__(self):
+		Self.sumPriceSize = {}
+		Self.sumSize = {}
+	def update(self, name, price, size):
+		if name not in self.sumPriceSize:
+            self.sumPriceSize[name] = 0
+            self.sumSize[name] = 0
+        self.sumPriceSize[name] += price * size
+        self.sumSize[name] += size
     def getavgprice(self, name):
-        if name in self.stockdict:
-            return self.stockdict[name]/self.sizelot[name]
-        else:
-            print('ticker do not exist yet')
+        if name not in self.sumPriceSize:
+            return None
+        return self.sumPriceSize[name]/self.sumSize[name]
 
 
-
-data = [
-    ('RIL', 100, 10 ),
-    ('RIL', 100, 10 ),
-    ('RIL', 100, 10 ),
-]
-
-while(1): #we are receiving stream
-    Stockstat = Stockstat()
-    stockstat.update('RIL', 100, 10)
-    avg = stockstat.getavgprice('RIL')
-    print('Avg price = ', avg)
+        
