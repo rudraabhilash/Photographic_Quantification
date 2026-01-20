dec1c76fe0f768d7608584354f447708b5dd1957
Abhilash Rudra <rudraabhilash@users.noreply.github.com>
2026-01-20 13:24:28 +0000

    client for ThreadSafeStockVwap

--- try.py
+++ try.py
@@ -75,3 +75,60 @@ class ThreadSafeStockVwap:
 #since updates are tiny(two dictionary ops) therefore lock contention should be minimal. 
 # HFT systems later shard by symbol or CPU core.
 
+#client code
+import random
+import time
+
+vwap_engine = ThreadSafeStockVwap()
+def market_feed(symbol: str):
+    for _ in range(1000):
+        price = random.uniform(100, 200)
+        size = random.randint(1, 1000)
+        vwap_engine.update(symbol, price, size)
+        time.sleep(random.uniform(0.001, 0.01))
+
+threads = [
+    threading.Thread(target=market_feed, args=("RIL",)),
+    threading.Thread(target=market_feed, args=("TCS",)),
+]
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
+print("RIL VWAP:", vwap_engine.getVwap("RIL"))
+
+
+import asyncio
+class AsyncStockVwap:
+    """ 
+    Async version - used when WebSockets or FIX gateways or Async market-data pipelines are used.
+    Volume weighted average price calculator 
+    for different stock names using streaming updates.
+    """
+    __slots__ = ('_sumPriceSize', '_sumSize', '_lock')
+
+    def __init__(self) -> None:
+        self._sumPriceSize: Dict[str, float] = {}
+        self._sumSize: Dict[str, int] = {}
+        self._lock = asyncio.Lock()
+
+    async def update(self, symbol: str, price: float, size: int) -> None:
+        if size <= 0:
+            raise ValueError("Trade size must be positive")
+
+        symbol = symbol.upper()
+        async with self._lock:
+            if symbol not in self._sumPriceSize:
+                self._sumPriceSize[symbol] = 0.0
+                self._sumSize[symbol]      = 0
+
+            self._sumPriceSize[symbol] += price * size
+            self._sumSize[symbol] += size
+
+    async def getVwap(self, symbol: str) -> float:
+        symbol = symbol.upper()
+        async with self._lock:
+            if symbol not in self._sumPriceSize:
+                raise KeyError(f"unknown symbol: {symbol}")
+            return self._sumPriceSize[symbol] / self._sumSize[symbol]
+            
