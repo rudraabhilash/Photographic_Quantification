b10d726a04c1b1e258e0915f5fdb676c66de08fc
Abhilash Rudra <rudraabhilash@users.noreply.github.com>
2026-01-21 15:29:10 +0000

    cdf,pdf,ppf

--- design_pattern.py
+++ design_pattern.py
@@ -44,3 +44,54 @@ if __name__ == "__main__":
     trade2 = Trade(1_000_000, FuturesFee())
     print(trade1.transaction_cost())  # 1000.0
     print(trade2.transaction_cost())  # 20
+
+
+# Problem: Facade design pattern
+# A trade execution involves many subsystems:
+# Risk check
+# Margin check
+# Order placement
+# Settlement
+# Client should not deal with this complexity.
+
+# Subsystems
+class RiskSystem:
+    def check_risk(self, amount):
+        return amount < 5_000_000
+
+
+class MarginSystem:
+    def check_margin(self, amount):
+        return amount * 0.2  # 20% margin required
+
+
+class OrderSystem:
+    def place_order(self, amount):
+        return f"Order placed for ₹{amount}"
+
+
+# Facade
+class TradingFacade:
+    def __init__(self):
+        self.risk = RiskSystem()
+        self.margin = MarginSystem()
+        self.order = OrderSystem()
+
+    def execute_trade(self, amount):
+        if not self.risk.check_risk(amount):
+            return "Trade rejected: Risk limit exceeded"
+
+        margin_required = self.margin.check_margin(amount)
+        confirmation = self.order.place_order(amount)
+
+        return {
+            "status": "SUCCESS",
+            "margin_required": margin_required,
+            "confirmation": confirmation
+        }
+trading = TradingFacade()
+
+result = trading.execute_trade(1_000_000)
+print(result)
+# Output:
+# {'status': 'SUCCESS', 'margin_required': 200000.0, 'confirmation': 'Order placed for ₹1000000'}
